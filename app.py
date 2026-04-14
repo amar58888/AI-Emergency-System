f36ca6798f1dc0f26316ee34bed3a4747726e7f3
Amar Jyoth <ajuamarjyoth@gmail.com>
2026-04-14 13:06:25 +0530

    fixed twilio secrets

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from firebase_admin import credentials, firestore
 
 # ---------------- CONFIG ----------------
 
-ACCOUNT_SID = "AC8007b7f07904e1536bd4148e18076ebd"
-AUTH_TOKEN = "27c84e4cdb80df775c25b82c941c67e0"
-TWILIO_NUMBER = "+17655725543"
-TARGET_NUMBER = "+918848328933"
+ACCOUNT_SID = st.secrets["AC8007b7f07904e1536bd4148e18076ebd"]
+AUTH_TOKEN = st.secrets["27c84e4cdb80df775c25b82c941c67e0"]
+TWILIO_NUMBER = st.secrets["+17655725543"]
+TARGET_NUMBER = st.secrets["+918848328933"]
 
 EMAIL = st.secrets["EMAIL"]
 APP_PASSWORD = st.secrets["APP_PASSWORD"]
